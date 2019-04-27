c3cfdd1d8378ae3c8c0446e0554991ba0bfcb756
Ty <tchristensen@mines.edu>
2019-04-27 16:56:41 -0600

    Added code that prints the names of all bakeries in dataset

--- project9.py
+++ project9.py
@@ -1,3 +1,10 @@
 import json
 
-with open('data.txt') as json_file:
+with open('business.json', encoding='utf8') as json_file:
+    entry = json_file.readline()
+    obj = json.loads(entry)
+    while entry:
+        if(str(obj['categories']).find('Bakeries') != -1):
+            print(obj['name'])
+        entry = json_file.readline()
+        obj = json.loads(entry)
